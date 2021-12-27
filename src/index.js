b0e7906ebe796cd91467a42f3afc91359554275d
4bpb <blakebein@gmail.com>
2021-12-27 14:25:50 -0700

    Added code for icon and art

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname,'frontend/assets/couponer.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
